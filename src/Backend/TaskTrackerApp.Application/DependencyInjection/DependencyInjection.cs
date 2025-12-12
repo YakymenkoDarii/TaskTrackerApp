6d2c3723c63f1d864501d79a0fc2c97bb13c78d7
Darii <yakymenkodarii@gmail.com>
2025-12-12 12:57:21 +0200

    Added files with dependency injection and Swagger

---
+++ src/Backend/TaskTrackerApp.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskTrackerApp.Application.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(AssemblyReference.AssemblyReference.Assembly));
+
+        return services;
+    }
+}
---
+++ src/Backend/TaskTrackerApp.Persistence/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskTrackerApp.Application.Interfaces.Repositories;
+using TaskTrackerApp.Application.Interfaces.UoW;
+using TaskTrackerApp.Persistence.Contexts;
+using TaskTrackerApp.Persistence.Repositories;
+using TaskTrackerApp.Persistence.UoW;
+
+namespace TaskTrackerApp.Persistence.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContextFactory<TaskTrackerDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddDbContext<TaskTrackerDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddScoped<ICardRepository, CardRepository>();
+        services.AddSingleton<IUnitOfWorkFactory, UnitOfWorkFactory>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
