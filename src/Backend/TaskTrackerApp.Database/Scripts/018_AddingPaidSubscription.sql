e0020fea8f47d9a90aa25cb900927566af7a672f
Darii <yakymenkodarii@gmail.com>
2026-02-16 23:14:11 +0200

    updated db

---
+++ src/Backend/TaskTrackerApp.Database/Scripts/018_AddingPaidSubscription.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE Users
+ADD IsPro BIT NOT NULL DEFAULT 0,
+StripeCustomerId NVARCHAR(50) NULL,
+SubscriptionEndDate DATETIME2 NULL;
+
+CREATE UNIQUE INDEX IX_Users_StripeCustomerId 
+ON Users(StripeCustomerId)
+WHERE StripeCustomerId IS NOT NULL;
